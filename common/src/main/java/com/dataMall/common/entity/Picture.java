afcb70be62e033d3a0ffa7f6945a5fb45e30ff8f
wooov <wooovi@126.com>
2025-01-25 01:09:04 +0800

    2025/1/25 完成图库模块，优化用户中心逻辑，对用户中心新增事务

---
+++ common/src/main/java/com/dataMall/common/entity/Picture.java
@@ -0,0 +1,97 @@
+package com.dataMall.common.entity;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableLogic;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+/**
+ * <p>
+ * 
+ * </p>
+ *
+ * @author woov
+ * @since 2025-01-23
+ */
+@Data
+@EqualsAndHashCode(callSuper = false)
+@Accessors(chain = true)
+@TableName("picture")
+public class Picture implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * id
+     */
+    @TableId(value = "id", type = IdType.AUTO)
+    private Long id;
+
+    /**
+     * 图片名称
+     */
+    private String picName;
+    /**
+     * 图片 url
+     */
+    private String url;
+
+    /**
+     * 图片大小
+     */
+    private Long picSize;
+
+    /**
+     * 图片宽度
+     */
+    private Integer picWidth;
+
+    /**
+     * 图片高度
+     */
+    private Integer picHeight;
+
+    /**
+     * 图片格式
+     */
+    private String picFormat;
+
+    /**
+     * 图片宽高比
+     */
+    private double picScale;
+    
+    /**
+     * 唯一标识
+     */
+    private String key;
+
+    /**
+     * 用户 id
+     */
+    private Integer uid;
+
+    /**
+     * 创建时间
+     */
+    private LocalDateTime createTime;
+
+    /**
+     * 更新时间
+     */
+    private LocalDateTime updateTime;
+
+    /**
+     * 状态：-1假删，0-正常
+     */
+    @TableLogic
+    private Integer state;
+
+
+}
